75b631869ba1eb1fa95f0ee9b60fbc53f9680b38
Aswinievanjeline <evanjelineaswini@gmail.com>
2025-06-30 18:55:18 +0530

    login

--- mrfranchise_adminpannel/src/main.jsx
+++ mrfranchise_adminpannel/src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import App from "./App.jsx"
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
